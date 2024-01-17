9e0d1a31cd64da716c017f5245965c9ca470a377
OniX <gauthier.delzenne@gmail.com>
2024-01-17 15:58:09 +0100

    New Generator P1

--- src/app/pages/generator/generator.component.ts
+++ src/app/pages/generator/generator.component.ts
@@ -4,10 +4,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatStepperModule } from '@angular/material/stepper';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
-import { MatSelectModule } from '@angular/material/select';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDividerModule } from '@angular/material/divider';
-import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
 
 import Papa from 'papaparse';
 import QRCode from 'qrcode';
@@ -26,6 +33,8 @@ import JSZip from 'jszip';
     FormsModule,
     ReactiveFormsModule,
     MatDividerModule,
+    MatTableModule,
+    MatInputModule,
   ],
   templateUrl: './generator.component.html',
   styleUrl: './generator.component.scss',
@@ -39,7 +48,7 @@ import JSZip from 'jszip';
 export class GeneratorComponent {
   constructor() {}
 
-  // STEP 1 - FILE
+  //#region VARIABLES
 
   selectedFile: any = null;
 
@@ -47,11 +56,16 @@ export class GeneratorComponent {
   flColumns = new FormControl(null, [Validators.required]);
   slColumns = new FormControl(null, [Validators.required]);
 
+  sourceColumns: string[] = [];
   columns: Column[] = [];
 
   data: unknown[] = [];
 
   columnsValid: boolean = false;
+
+  //#endregion
+
+  //#region STEP 1 - FILE
 
   onFileSelected(event: any): void {
     this.previewDone = false;
@@ -64,43 +78,156 @@ export class GeneratorComponent {
         complete: ({ data }) => {
           const cols: string[] = data[0] as string[];
 
-          this.columns = cols.map((name: string) => ({ name }));
+          this.sourceColumns = cols.map((name: string) => name);
           this.data = data.slice(1);
+          this.onLabelEdited();
         },
       });
     }
   }
 
-  // STEP 2 - COLUMNS
+  //#endregion
 
-  onColumnsSubmit(): void {
-    this.previewDone = false;
-    if (this.qrColumns.valid && this.flColumns.valid && this.slColumns.valid) {
-      this.columnsValid = true;
-    }
+  //#region STEP 2 - COLUMNS & FORMAT
+
+  displayedColumns: string[] = ['name', 'type', 'x', 'y', 'height', 'width'];
+
+  label: Label = {
+    height: 96,
+    width: 200,
+    columns: [],
+  };
+
+  labelSrc: string = '';
+
+  addColumn(): void {
+    this.label.columns = [
+      ...this.label.columns,
+      {
+        name: this.sourceColumns[0],
+        type: 'text',
+        x: 0,
+        y: 0,
+        height: 0,
+        width: 0,
+      },
+    ];
   }
 
-  // STEP 3 - PREVIEW
+  deleteColumn(index: number): void {
+    this.label.columns.splice(index, 1);
+    this.onLabelEdited();
+  }
+
+  async onLabelEdited(): Promise<void> {
+    // Generate label
+    const canvas = new OffscreenCanvas(this.label.width, this.label.height);
+
+    const ctx = canvas.getContext('2d');
+
+    if (!ctx) return;
+
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    const columns = [...this.label.columns];
+
+    const processNext = async (): Promise<void> => {
+      if (columns.length === 0) {
+        // setTimeout(async () => {
+          console.log('canvas conv started');
+          let blob = await canvas.convertToBlob();
+          this.labelSrc = URL.createObjectURL(blob);
+          console.log('canvas converted');
+          console.log(this.labelSrc);
+
+          this.previewDone = false;
+          if (
+            this.qrColumns.valid &&
+            this.flColumns.valid &&
+            this.slColumns.valid
+          ) {
+            this.columnsValid = true;
+          }
+        // }, 2000);
+        return;
+      }
+
+      const col = columns.shift() as Column;
+
+      if (col.type === 'text') {
+        let v = (this.data[0] as string[])[
+          this.sourceColumns.indexOf(col.name)
+        ] as string;
+        ctx.font = `${col.height * 0.75}px Arial`;
+        ctx.fillStyle = '#000';
+        ctx.fillText(v, col.x, col.y + col.height, col.width);
+
+        console.log('text drawn');
+
+        processNext();
+      }
+
+      if (col.type === 'img') {
+        let v = ('data:image/png;base64, ' +
+          (this.data[0] as string[])[
+            this.sourceColumns.indexOf(col.name)
+          ]) as string;
+        const img = new Image();
+
+        img.onload = () => {
+          ctx.drawImage(img, col.x, col.y, col.width, col.height);
+
+          console.log('img drawn');
+
+          processNext();
+        };
+        img.src = v;
+      }
+
+      if (col.type === 'qr') {
+        let v = (this.data[0] as string[])[
+          this.sourceColumns.indexOf(col.name)
+        ] as string;
+        QRCode.toDataURL(v, (err, url) => {
+          const img = new Image();
+
+          console.log('url : ', url);
+
+          img.onload = () => {
+            ctx.drawImage(img, col.x, col.y, col.width, col.height);
+
+            console.log('qr drawn');
+
+            processNext();
+          };
+          img.src = url;
+        });
+      }
+    };
+
+    processNext();
+  }
+
+  //#endregion
+
+  //#region STEP 3 - PREVIEW
 
   previewDone: boolean = false;
 
   offCanvas = new OffscreenCanvas(10, 10);
   offCtx = this.offCanvas.getContext('2d');
 
-  src: string = '';
+  previewSrc: string = '';
 
   onPreviewClick(): void {
     const fr = this.data[0] as string[];
 
-    const qrIndex = (this.qrColumns.value as unknown) as number;
-    const flIndex = (this.flColumns.value as unknown) as number;
-    const slIndex = (this.slColumns.value as unknown) as number;
+    const qrIndex = this.qrColumns.value as unknown as number;
+    const flIndex = this.flColumns.value as unknown as number;
+    const slIndex = this.slColumns.value as unknown as number;
 
-    const [qr, ref, nom] = [
-      fr[qrIndex],
-      fr[flIndex],
-      fr[slIndex],
-    ];
+    const [qr, ref, nom] = [fr[qrIndex], fr[flIndex], fr[slIndex]];
 
     QRCode.toDataURL(qr, (err, url) => {
       // Transform to image
@@ -135,7 +262,7 @@ export class GeneratorComponent {
 
         // Draw preview
         let blob = await canvas.convertToBlob();
-        this.src = URL.createObjectURL(blob);
+        this.previewSrc = URL.createObjectURL(blob);
 
         // Done
         this.previewDone = true;
@@ -145,7 +272,10 @@ export class GeneratorComponent {
     });
   }
 
-  // STEP 4 - DOWNLOAD
+  //#endregion
+
+  //#region STEP 4 - DOWNLOAD
+
   download(): void {
     const zip = new JSZip();
 
@@ -216,4 +346,6 @@ export class GeneratorComponent {
       });
     });
   }
+
+  //#endregion
 }
